a27a8e7ad0891f2db7360faf5722b9b1f7fd9bf7
Piotr Warcholek <petricho@wp.pl>
2023-03-07 16:00:04 +0100

    f added docker

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0", // needed for the Docker Container port mapping to work
+    port: 3000, // you can replace this port with any port
+  }
+})
