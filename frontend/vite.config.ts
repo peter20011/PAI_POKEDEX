c8fa840996b8898757593a1d7654b25810d54120
peter20011 <petricho@wp.pl>
2023-05-20 20:59:48 +0200

    some changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", 
-    port: 3000
-  }
-})
+    host: true, 
+    strictPort: true,
+    port: 3000, // Replace with any desired port
+  },
+});
